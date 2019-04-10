150b00b2cbd092f306602dfe7977727df0c1fa3e
Karim Alibhai <karim@alibhai.co>
2019-04-09 22:39:25 -0400

    Add: initial

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log(
+  {
+    name: 'Karim Alibhai',
+    email: 'karim@alibhai.co',
+    github: 'https://github.com/karimsa',
+    medium: 'https://medium.com/@karimsa',
+    website: 'https://alibhai.co',
+    tagline: 'Here\'s to the crazy ones...',
+  }
+)
+
